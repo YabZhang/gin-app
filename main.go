c1a9420d19b04829d04b5d08475110377e890f5a
yabinzhang <ybzhang2015@gmail.com>
2020-05-06 16:55:20 +0800

    extract route func

--- main.go
+++ main.go
@@ -1,33 +1,37 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+var router *gin.Engine
 
 func main() {
 	// set the default router
-	router := gin.Default()
+	router = gin.Default()
 
 	// load templates
 	router.LoadHTMLGlob("templates/*")
 
 	// define route handlers
-	router.GET("/", func(c *gin.Context) {
-		// call the HTML method of the Content to render a template
-		c.HTML(
-			// set http status code
-			http.StatusOK,
-			// use the index.html template
-			"index.html",
-			// pass the data that the page uses
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-	})
+	initializeRoutes()
 
 	// start to run
 	router.Run()
+}
+
+
+func showIndexPage(c *gin.Context) {
+	// call the HTML method of the Content to render a template
+	c.HTML(
+		// set http status code
+		http.StatusOK,
+		// use the index.html template
+		"index.html",
+		// pass the data that the page uses
+		gin.H{
+			"title": "Home Page",
+		},
+	)
 }
---
+++ routes.go
@@ -0,0 +1,8 @@
+package main
+
+func initializeRoutes() {
+	// index page
+	router.GET("/", showIndexPage)
+}
+
+
