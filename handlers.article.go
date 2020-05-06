9329d983f219af6a0fa71898b0dff67d64d71c03
yabinzhang <ybzhang2015@gmail.com>
2020-05-06 17:29:24 +0800

    article detail

--- handlers.article.go
+++ handlers.article.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func showIndexPage(c *gin.Context) {
@@ -20,4 +21,27 @@ func showIndexPage(c *gin.Context) {
 			"payload": articles,
 		},
 	)
+}
+
+func getArticle(c *gin.Context) {
+	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+		if article, err := getArticleByID(articleID); err == nil {
+			// call the HTML method of the Content to render a template
+			c.HTML(
+				// set http status code
+				http.StatusOK,
+				// use the index.html template
+				"article.html",
+				// pass the data that the page uses
+				gin.H{
+					"title": article.Title,
+					"payload": article,
+				},
+			)
+		} else {
+			c.AbortWithError(http.StatusNotFound, err)
+		}
+	} else {
+		c.AbortWithError(http.StatusNotFound, err)
+	}
 }
--- routes.go
+++ routes.go
@@ -3,6 +3,9 @@ package main
 func initializeRoutes() {
 	// index page
 	router.GET("/", showIndexPage)
+
+	// single article detail
+	router.GET("/article/view/:article_id", getArticle)
 }
 
 
